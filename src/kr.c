ewd340/kr

commit ee8a06c1889f2a4927f337b9f3f094f2d17333e8

Only MODE_ENCRYPT and MODE_DECRYPT require either a passphrase or a key.
Seeing it this way, it is easier to display the usage text when no
passphrase nor a keyfile are used in these modes.

src/kr.c
@@ -643,7 +643,7 @@ int main(int argc, char *argv[])
         if (err != ERR_OK) {
             BAIL(err);
         }
-    } else if (!use_passphrase && mode != MODE_KEYGEN && mode != MODE_VERSION) {
+    } else if (mode & (MODE_ENCRYPT | MODE_DECRYPT)) {
         mode = MODE_USAGE;
     }
 

commit 1fd7e27a8eaf74a5ab12733fb34e1a95ba1f8c24

FIX: Commit ee8a06c introduced a regression. This one fixes it.
Commit #ee8a06c actually disabled passphras-ebased encryption and
decryption operations. The mode should be reset to MODE_USAGE only when
the mode is either MODE_ENCRYPT or MODE_DECRYPT *AND* use_passphrase is
false (0).

src/kr.c
@@ -648,7 +648,7 @@ int main(int argc, char *argv[])
         if (err != ERR_OK) {
             BAIL(err);
         }
-    } else if (mode & (MODE_ENCRYPT | MODE_DECRYPT)) {
+    } else if (!use_passphrase && (mode & (MODE_ENCRYPT | MODE_DECRYPT))) {
         mode = MODE_USAGE;
     }
 